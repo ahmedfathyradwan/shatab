c919de8458a726f35f74d7299b5218d71be6444a
Ahmed Fathy <ahmedfathyradwan1@gmail.com>
2025-08-19 03:44:02 +0300

    Fix font paths and move fonts to public/

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,13 @@ import AppWrapper from "./components/AppWrapper";
 // Local fonts - Changa
 const changa = localFont({
   src: [
-    { path: "/fonts/Changa/Changa-ExtraLight.woff2", weight: "200", style: "normal" },
-    { path: "/fonts/Changa/Changa-Light.woff2",      weight: "300", style: "normal" },
-    { path: "/fonts/Changa/Changa-Regular.woff2",    weight: "400", style: "normal" },
-    { path: "/fonts/Changa/Changa-Medium.woff2",     weight: "500", style: "normal" },
-    { path: "/fonts/Changa/Changa-SemiBold.woff2",   weight: "600", style: "normal" },
-    { path: "/fonts/Changa/Changa-Bold.woff2",       weight: "700", style: "normal" },
-    { path: "/fonts/Changa/Changa-ExtraBold.woff2",  weight: "800", style: "normal" },
+    { path: "./fonts/Changa/Changa-ExtraLight.woff2", weight: "200", style: "normal" },
+    { path: "./fonts/Changa/Changa-Light.woff2",      weight: "300", style: "normal" },
+    { path: "./fonts/Changa/Changa-Regular.woff2",    weight: "400", style: "normal" },
+    { path: "./fonts/Changa/Changa-Medium.woff2",     weight: "500", style: "normal" },
+    { path: "./fonts/Changa/Changa-SemiBold.woff2",   weight: "600", style: "normal" },
+    { path: "./fonts/Changa/Changa-Bold.woff2",       weight: "700", style: "normal" },
+    { path: "./fonts/Changa/Changa-ExtraBold.woff2",  weight: "800", style: "normal" },
   ],
   variable: "--font-changa",
   display: "swap",
@@ -20,13 +20,13 @@ const changa = localFont({
 // Local fonts - Poppins
 const poppins = localFont({
   src: [
-    { path: "/fonts/Poppins/Poppins-Light.woff2",     weight: "300", style: "normal" },
-    { path: "/fonts/Poppins/Poppins-Regular.woff2",   weight: "400", style: "normal" },
-    { path: "/fonts/Poppins/Poppins-Medium.woff2",    weight: "500", style: "normal" },
-    { path: "/fonts/Poppins/Poppins-SemiBold.woff2",  weight: "600", style: "normal" },
-    { path: "/fonts/Poppins/Poppins-Bold.woff2",      weight: "700", style: "normal" },
-    { path: "/fonts/Poppins/Poppins-ExtraBold.woff2", weight: "800", style: "normal" },
-    { path: "/fonts/Poppins/Poppins-Black.woff2",     weight: "900", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-Light.woff2",     weight: "300", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-Regular.woff2",   weight: "400", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-Medium.woff2",    weight: "500", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-SemiBold.woff2",  weight: "600", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-Bold.woff2",      weight: "700", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-ExtraBold.woff2", weight: "800", style: "normal" },
+    { path: "./fonts/Poppins/Poppins-Black.woff2",     weight: "900", style: "normal" },
   ],
   variable: "--font-poppins",
   display: "swap",
